9c68aece96e69bbe9c7010618e60cdf5f0732d80
sabbirtanvir13 <sabbirtanvirahmed18@gmail.com>
2026-07-09 19:13:53 +0600

    Fix catchAsync error masking

--- src/utlis/catchAsync.ts
+++ src/utlis/catchAsync.ts
@@ -7,12 +7,7 @@ export const catchAsync = (fn: RequestHandler) => {
     try {
       await fn(req, res, next);
     } catch (error) {
-      return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-        success: false,
-        statusCode: httpStatus.INTERNAL_SERVER_ERROR,
-        message: "Something went wrong",
-        error: (error as Error).message,
-      });
+      next(error);
     }
   };
 };
