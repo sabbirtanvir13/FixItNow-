9b6d8fe0d8ed622ddac769758c324b5dc1baa525
sabbirtanvir13 <sabbirtanvirahmed18@gmail.com>
2026-07-09 20:38:50 +0600

    fix vercel

--- src/server.ts
+++ src/server.ts
@@ -1,62 +1,27 @@
-// import app from "./app";
-// import { prisma } from "./lib/prisma";
-// import config from "./config";
-// const PORT = config.PORT ;
-// async function main() {
-//     try {
-   
-//       await prisma.$connect();
-//        console.log("Connected to the database successfully.");
-//       app.listen(PORT, () => {
-//         console.log(`Server is running on port ${PORT}`);
-//       });
-    
-//     } catch (error) {
-        
-//       console.error("Error connecting to the database:", error);
-//       await prisma.$disconnect();
-//         process.exit(1);
- 
-  
-//     }
-// }
-
-// main();
-
-
-
 import app from "./app";
 import { prisma } from "./lib/prisma";
-
-
-// Vercel serverless export
-export default app;
-
-
-// Local development এর জন্য
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 5000;
-
-  async function main() {
+import config from "./config";
+const PORT = config.PORT ;
+async function main() {
     try {
-
+   
       await prisma.$connect();
-
-      console.log("Database connected successfully");
-
+       console.log("Connected to the database successfully.");
       app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
+        console.log(`Server is running on port ${PORT}`);
       });
-
+    
     } catch (error) {
-
-      console.error("Database connection failed", error);
-
+        
+      console.error("Error connecting to the database:", error);
       await prisma.$disconnect();
-
-      process.exit(1);
+        process.exit(1);
+ 
+  
     }
-  }
-
-  main();
 }
+
+main();
+
+
+
